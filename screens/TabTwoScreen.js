527ebe5255957d9ac6a9461061477550ec395b97
namnguyen080400 <namn.nguyen117@gmail.com>
2024-05-29 16:15:05 -0700

    Add more textboxes

--- screens/TabTwoScreen.js
+++ screens/TabTwoScreen.js
@@ -1,38 +1,77 @@
 
 import React from 'react';
-import { StyleSheet, Button, TextInput } from "react-native";
+import { StyleSheet, Button, TextInput, ScrollView } from "react-native";
 
 import EditScreenInfo from "../components/EditScreenInfo";
 import { Text, View} from "../components/Themed";
 
 export default function TabTwoScreen({ navigation }) {
 
-  const [text, onChangeText] = React.useState('');
+  const [text1, text2, text3, text4, text5, text6, onChangeText] = React.useState('');
   
   return (
-    <View style={styles.container}>
+    <ScrollView>
+      <View style={styles.container}>
 
-      
-      <Text style={styles.title}>Create Entry</Text>
-      
-      <Button onPress={navigation.openDrawer} title="Location"/>
+        <Text style={styles.title}>Create Entry</Text>
+        
+        <Button onPress={navigation.openDrawer} title="Location"/>
 
-      <Button onPress={navigation.openDrawer} title="Image"/>
+        <Button onPress={navigation.openDrawer} title="Image"/>
 
-      <TextInput
-        style={styles.input}
-        onChangeText={onChangeText}
-        value={text}
-      />
 
-      <View
-        style={styles.separator}
-        lightColor="#eee"
-        darkColor="rgba(255,255,255,0.1)"
-      />
-      
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
+        <Text style={styles.text}>Address</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text1}
+        />
+        
+        <Text style={styles.text}>Title</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text2}
+        />     
+
+        <Text style={styles.text}>Author</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text3}
+        />
+
+        <Text style={styles.text}>Rating</Text>
+        <TextInput
+          style={styles.input}
+          keyboardType='numeric'
+          onChangeText={onChangeText}
+          value={text4}
+        />
+
+        <Text style={styles.text}>Description</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text5}
+        />
+        
+        <Text style={styles.text}>Tags</Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeText}
+          value={text6}
+        />        
+
+        <View
+          style={styles.separator}
+          lightColor="#eee"
+          darkColor="rgba(255,255,255,0.1)"
+        />
+        
+        <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+      </View>
+    </ScrollView>
   );
 }
 
