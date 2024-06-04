ec2fe7491e6a6c67b3b5c1975471cc972674b4a1
Hammaad-M <hammaad.memon@gmail.com>
2024-06-04 14:40:28 -0700

    add /getCoordinates route

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // create node.js and express server that listens on port 3000
-// and responds with a message "Hello World" when a request is made to the root URL
 const express = require("express");
+const axios = require("axios");
 const app = express();
 const port = 3000;
 
@@ -11,13 +11,38 @@ app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
+const GOOGLE_MAPS_API_KEY = "AIzaSyAFcBA5PGa8mPKp9WZqs23rtDYsN4F4Uwo";
+
+const getCoords = async (addressString) => {
+  let status;
+  try {
+    const response = await axios.get(
+      `https://maps.googleapis.com/maps/api/geocode/json`,
+      {
+        params: {
+          address: addressString,
+          key: GOOGLE_MAPS_API_KEY,
+        },
+      }
+    );
+    status = response.data.status;
+    const location = response.data.results[0].geometry.location;
+    return [location.lat, location.lng];
+  } catch (error) {
+    console.error("Error fetching geocoding data:", status);
+  }
+};
+// EXAMPLE USAGE
+// getCoords("Bellevue").then(console.log);
+// getCoords("3000 Landerholm Circle SE, Bellevue, WA 98007").then(console.log);
+
 app.get("/", (req, res) => {
   res.send("Try /getParkings or /setParking instead! ");
 });
 
 app.get("/getParkings", (req, res) => {
   // TODO: get parkings from db, sort using address, and return them as json
-  db.find({address:req.body}, (err, docs) => {
+  db.find({ address: req.body }, (err, docs) => {
     res.json(docs);
   });
 
@@ -26,6 +51,7 @@ app.get("/getParkings", (req, res) => {
     res.json(docs);
   });
 });
+
 app.post("/setParking", (req, res) => {
   const newParking = req.body;
   db.insert(newParking, function (err, newDoc) {
@@ -37,6 +63,16 @@ app.post("/setParking", (req, res) => {
       res.status(200).send({ _id: newDoc._id });
     }
   });
+});
+
+app.get("/getCoordinates", async (req, res) => {
+  const addressString = req.query.address;
+  if (!addressString) {
+    return res.status(400).send({ error: "Address string is required" });
+  }
+
+  const [x, y] = await getCoords(addressString);
+  res.json({ x, y });
 });
 
 /**var newParking = {
