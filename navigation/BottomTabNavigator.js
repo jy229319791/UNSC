c20e2b5730b347ee53307a8eaf44a9997cd23a60
namnguyen080400 <namn.nguyen117@gmail.com>
2024-06-05 19:50:18 -0700

    Add bike parking page

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -8,6 +8,7 @@ import { useColorScheme } from "react-native";
 import Colors from "../constants/Colors";
 import TabOneScreen from "../screens/TabOneScreen";
 import TabTwoScreen from "../screens/TabTwoScreen";
+import TabThreeScreen from "../screens/TabThreeScreen";
 
 const BottomTab = createBottomTabNavigator();
 
@@ -39,6 +40,16 @@ export default function BottomTabNavigator() {
           ),
         }}
       />
+      <BottomTab.Screen
+        name="TabThree"
+        component={TabThreeNavigator}
+        options={{
+          headerShown: false,
+          tabBarIcon: ({ color }) => (
+            <TabBarIcon name="ios-code" color={color} />
+          ),
+        }}
+      />     
     </BottomTab.Navigator>
   );
 }
@@ -76,5 +87,19 @@ function TabTwoNavigator() {
         options={{ headerTitle: "Create Bike Entry" }}
       />
     </TabTwoStack.Navigator>
+  );
+}
+
+const TabThreeStack = createStackNavigator();
+
+function TabThreeNavigator() {
+  return (
+    <TabThreeStack.Navigator>
+      <TabThreeStack.Screen
+        name="TabThreeScreen"
+        component={TabThreeScreen}
+        options={{ headerTitle: "Bike Parking" }}
+      />
+    </TabThreeStack.Navigator>
   );
 }
