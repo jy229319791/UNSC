c20574cc32e24927c0aa7011eb06998b7f694cbb
MBal2 <63755536+MBal2@users.noreply.github.com>
2024-06-07 22:45:20 -0700

    F4 complete

--- screens/TabOneScreen.js
+++ screens/TabOneScreen.js
@@ -7,28 +7,56 @@ import { ScrollView } from "react-native-gesture-handler";
 export default function TabOneScreen({ navigation }) {
 
   const [text1, onChangeText] = React.useState('');
-
+  const bikeParkings = [{
+    title: "Bellevue College Bike Parking",
+    rating: "3.9",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["Clean","Camaras nearby"]
+  },
+  {
+    title: "My Garage Bike Parking",
+    rating: "1.7",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["High theft"]
+  },
+  {
+    title: "Crossroads Bike Parking",
+    rating: "4",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    tags: ["Clean","Low crime"]
+  },
+  ];
   return (
     <ScrollView>
       <View style={styles.container}>
-        <Text style={styles.title}>Find</Text>
-        
+        <Text style={styles.title1}>Find</Text>
+
         <Text style={styles.Text}>Bike Parking</Text>
         <TextInput
           style={styles.input}
           onChangeText={onChangeText}
           value={text1}
         />
-        <Button onPress={Search} title="Search"/>
+        <Button onPress={Search} title="Search" />
         <Text style={styles.Text}></Text>
         <View
           style={styles.separator}
           lightColor="#eee"
           darkColor="rgba(255,255,255,0.1)"
         />
-        <Button onPress={Geolocation} title="Geolocate"/>
-        <EditScreenInfo path="/screens/TabOneScreen.tsx" />        
-      </View>      
+        <Button onPress={Geolocation} title="Geolocate" />
+        <Text style={styles.Text}></Text>
+        <Text style={styles.Text}>Bike parkings near you</Text>
+        <Text style={styles.Text}></Text>
+        {bikeParkings.map((parking, index) => (
+          <View key={index}>
+            <Text style={styles.title2}>Title: {parking.title}</Text>
+            <Text>Rating: {parking.rating}</Text>
+            <Text>Description: {parking.description}</Text>
+            <Text>Tags: {parking.tags.join(', ')}</Text>
+          </View>
+        ))}
+      </View>
     </ScrollView>
   );
 }
@@ -39,7 +67,7 @@ const Search = () => {
   });
 };
 const Geolocation = () => {
-  
+
 };
 
 const styles = StyleSheet.create({
@@ -48,10 +76,14 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
-  title: {
+  title1: {
     fontSize: 40,
     fontWeight: "bold",
-  }, 
+  },
+  title2: {
+    fontSize: 20,
+    fontWeight: "bold",
+  },
   separator: {
     marginVertical: 30,
     height: 1,
